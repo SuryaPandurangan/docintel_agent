7104268743d2b2fba4de61b8355009b9edf27781
SuryaPandurangan <surya.panduranga@tigeranalytics.com>
2025-07-18 10:07:35 +0000

    handled multiple doc

--- app.py
+++ app.py
@@ -11,22 +11,30 @@ api_key = os.getenv("GOOGLE_API_KEY")
 st.set_page_config(page_title="DocIntel Agent", layout="wide")
 st.title("📄 DocIntel Agent")
 
-uploaded_file = st.file_uploader("Upload a PDF or DOCX file", type=["pdf", "docx"])
+# uploaded_file = st.file_uploader("Upload a PDF or DOCX file", type=["pdf", "docx"])
+uploaded_files = st.file_uploader(
+    "Upload one or more PDF/DOCX files",
+    type=["pdf", "docx"],
+    accept_multiple_files=True,
+)
 
-if uploaded_file:
-    with st.spinner("Reading document..."):
-        temp_path = f"temp/{uploaded_file.name}"
+if uploaded_files:
+    all_text = ""
+    for file in uploaded_files:
+        temp_path = f"temp/{file.name}"
         os.makedirs("temp", exist_ok=True)
         with open(temp_path, "wb") as f:
-            f.write(uploaded_file.read())
-        raw_text = load_document(temp_path)
-        st.success("Document loaded.")
-        st.text_area("Extracted Text", raw_text[:2000], height=300)
+            f.write(file.read())
+        text = load_document(temp_path)
+        st.success(f"{file.name} loaded.")
+        all_text += f"\n\n---\n\n{text}"  # Keep docs separated with delimiter
 
-        with st.spinner("Chunking & embedding..."):
-            chunks = [raw_text[i : i + 1000] for i in range(0, len(raw_text), 1000)]
-            vectorstore = create_vector_store(chunks)
-            qa_chain = build_qa_chain(vectorstore, api_key)
+    st.text_area("Combined Text Preview", all_text[:3000], height=300)
+
+    with st.spinner("Embedding all documents..."):
+        chunks = [all_text[i : i + 1000] for i in range(0, len(all_text), 1000)]
+        vectorstore = create_vector_store(chunks)
+        qa_chain = build_qa_chain(vectorstore)
 
         st.success("Ready to answer your questions!")
         user_q = st.text_input("Ask a question about the document:")
--- qa_chain.py
+++ qa_chain.py
@@ -2,7 +2,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import RetrievalQA
 
 
-def build_qa_chain(vectorstore, api_key):
+def build_qa_chain(vectorstore):
     llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-preview-05-20")
     chain = RetrievalQA.from_chain_type(
         llm=llm, retriever=vectorstore.as_retriever(), return_source_documents=True
